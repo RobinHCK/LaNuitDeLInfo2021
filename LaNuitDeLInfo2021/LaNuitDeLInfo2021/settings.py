ee7ace4154b5e071cc70942735c59d8eba45cd24
Baptiste Lafabregue <lafabregue@unistra.fr>
2021-12-02 15:49:15 +0100

    add toolbar

--- LaNuitDeLInfo2021/LaNuitDeLInfo2021/settings.py
+++ LaNuitDeLInfo2021/LaNuitDeLInfo2021/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig'
+    'debug_toolbar',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'LaNuitDeLInfo2021.urls'
@@ -129,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1']
--- LaNuitDeLInfo2021/LaNuitDeLInfo2021/urls.py
+++ LaNuitDeLInfo2021/LaNuitDeLInfo2021/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    url('admin/', admin.site.urls),
+    url('', include('home.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
